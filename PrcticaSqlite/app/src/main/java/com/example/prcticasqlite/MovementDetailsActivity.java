479c7af924672d5126fdb7113fac5be423c159a1
shengjunye <45043165+shengjunye@users.noreply.github.com>
2020-01-27 00:12:01 +0100

    layouts creats

---
+++ PrcticaSqlite/app/src/main/java/com/example/prcticasqlite/MovementDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.prcticasqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MovementDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movement_details);
+    }
+}
